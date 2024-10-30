24fa4bdea5f3ec6dc712dd1476236f46211f9fc0
akashstwt <akashpatel115347@gmail.com>
2024-10-30 13:22:43 +0530

    footer updated

--- lib/instagram_footer.dart
+++ lib/instagram_footer.dart
@@ -9,34 +9,38 @@ class InstagramFooter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      height: 70,
+    return Container(
+      height: 50,
       color: Colors.grey.shade400,
       child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.home, size: 32),
-              onPressed: () => _navigate(context, '/home'),
-            ),
-            IconButton(
-              icon: const Icon(Icons.search, size: 32),
-              onPressed: () => _navigate(context, '/search'),
-            ),
-            IconButton(
-              icon: const Icon(Icons.add_box, size: 32),
-              onPressed: () => _navigate(context, '/add'),
-            ),
-            IconButton(
-              icon: const Icon(Icons.favorite_border, size: 32),
-              onPressed: () => _navigate(context, '/favorites'),
-            ),
-            IconButton(
-              icon: const Icon(Icons.account_circle, size: 32),
-              onPressed: () => _navigate(context, '/profile'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.home, size: 32),
+                onPressed: () => _navigate(context, '/home'),
+              ),
+              IconButton(
+                icon: const Icon(Icons.search, size: 32),
+                onPressed: () => _navigate(context, '/search'),
+              ),
+              IconButton(
+                icon: const Icon(Icons.add_box, size: 32),
+                onPressed: () => _navigate(context, '/add'),
+              ),
+              IconButton(
+                icon: const Icon(Icons.favorite_border, size: 32),
+                onPressed: () => _navigate(context, '/favorites'),
+              ),
+              IconButton(
+                icon: const Icon(Icons.account_circle, size: 32),
+                onPressed: () => _navigate(context, '/profile'),
+              ),
+            ],
+          ),
         ),
       ),
     );
