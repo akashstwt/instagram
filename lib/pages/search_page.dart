bfec14076d8b43ac644f1899b9febb92533c1a60
akashstwt <akashpatel115347@gmail.com>
2025-01-23 01:21:15 +0530

    serch page updated

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/instagram_footer.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -12,6 +13,7 @@ class SearchPage extends StatelessWidget {
       body: const Center(
         child: Text('Search Page'),
       ),
+      bottomNavigationBar: const InstagramFooter(),
     );
   }
 }
