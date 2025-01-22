f148bf35fa0e4be9847b556e27fcc989a5c30c36
akashstwt <akashpatel115347@gmail.com>
2025-01-23 01:19:55 +0530

    footer issue solved

--- lib/instagram_footer.dart
+++ lib/instagram_footer.dart
@@ -1,48 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/pages/explore_page.dart';
+import 'package:instagram/pages/favorites_page.dart';
+import 'package:instagram/pages/home_page.dart';
+import 'package:instagram/pages/profile_page.dart';
+import 'package:instagram/pages/search_page.dart';
 
 class InstagramFooter extends StatelessWidget {
   const InstagramFooter({super.key});
 
-  void _navigate(BuildContext context, String route) {
-    Navigator.pushNamed(context, route);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      color: Colors.grey.shade400,
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.home, size: 32),
-                onPressed: () => _navigate(context, '/home'),
-              ),
-              IconButton(
-                icon: const Icon(Icons.search, size: 32),
-                onPressed: () => _navigate(context, '/search'),
-              ),
-              IconButton(
-                icon: const Icon(Icons.add_box, size: 32),
-                onPressed: () => _navigate(context, '/add'),
-              ),
-              IconButton(
-                icon: const Icon(Icons.favorite_border, size: 32),
-                onPressed: () => _navigate(context, '/favorites'),
-              ),
-              IconButton(
-                icon: const Icon(Icons.account_circle, size: 32),
-                onPressed: () => _navigate(context, '/profile'),
-              ),
-            ],
-          ),
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _getCurrentIndex(context),
+      onTap: (index) => _onItemTapped(index, context),
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
         ),
-      ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box_outlined),
+          label: 'Post',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_border),
+          label: 'Likes',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'Profile',
+        ),
+      ],
     );
+  }
+
+  int _getCurrentIndex(BuildContext context) {
+    final String currentRoute = ModalRoute.of(context)?.settings.name ?? '/';
+    switch (currentRoute) {
+      case '/search':
+        return 1;
+      case '/explore':
+        return 2;
+      case '/favorites':
+        return 3;    
+      case '/profile':
+        return 4;
+      default:
+        return 0;
+    }
+  }
+
+  void _onItemTapped(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Home')),
+        );
+        break;
+      case 1:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const SearchPage()),
+        );
+        break;
+      case 2:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) =>  ExplorePage()),
+        );
+        break;
+      case 3:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const  FavoritesPage()),
+        );
+        break;    
+      case 4:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const ProfilePage()),
+        );
+        break;
+    }
   }
 }
